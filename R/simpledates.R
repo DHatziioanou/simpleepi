e2b7da215858146971789e2749017dc498692b37
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2021-01-26 21:38:16 +0000

    rename simple dates

--- R/simpledates.R
+++ R/simpledates.R
@@ -18,7 +18,7 @@
 #' # Format all column from within a data.table where the columns contain the string date into date format
 #' for (col in (names(dt)[grepl("date", names(dt))])){dt[,(col) := simpledates(dt[,get(col)])]}
 #'@export
-simple_dates <- function(x){
+simpledates <- function(x){
  if(suppressWarnings(sum(!(is.na(lubridate::parse_date_time(x, orders = c("dmy", "mdy", "ymd")))))) == 0) {
       x <- as.numeric(x)
       x <- as.Date(x, origin = "1899-12-30")

46f92f13f4ed3609e2da28b35d29b6a9f9bfab6e
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2021-02-09 17:17:53 +0000

    isoweek simplify

--- R/simpleIsoweek.R
+++ R/simpleIsoweek.R
@@ -7,7 +7,7 @@
 #'
 #' @examples
 #' df$Isoweek <- simpleIsoweek(df$sample_date)
-#' 
+#'
 #' @export
 simpleIsoweek <- function(x, integer){
   yearweek <- paste0(lubridate::isoyear(x), stringr::str_pad(lubridate::isoweek(x), 2, pad = "0"))
@@ -19,18 +19,3 @@ simpleIsoweek <- function(x, integer){
 }
 
 
-
-#' Title Get Iso year-week from a date object
-#'
-#' @param x Date value or vector to convert to Iso year-week
-#'
-#' @return Returns Iso year-weeks as strings in the format YYYYWW
-#'
-#' @examples
-#' df$Isoweek <- simpleIsoweek(df$sample_date)
-#' 
-#' @export
-simpleIsoweek <- function(x, complete){
-  yearweek <- paste0(lubridate::isoyear(x), stringr::str_pad(lubridate::isoweek(x), 2, pad = "0"))
-  return(yearweek)
-}

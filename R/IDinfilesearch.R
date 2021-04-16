ea3cfbaaa1486648c6bed72d97de2a5c5f8aa924
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2021-04-16 15:41:45 +0100

    add IDinfilesearch

---
+++ R/IDinfilesearch.R
@@ -0,0 +1,34 @@
+#' Title Search a list of files for values in a consistent column
+#'
+#' @param IDs Id values to look for.
+#' @param IDcol Column containing IDs in files.
+#' @param files Files to look in
+#' @param complete Optional; retain files without matches in list or not. Default is FALSE
+#'
+#' @return Returns a list with file names and rows of data with listed IDs in the IDCol column
+#'
+#' @examples
+#'
+#' simplewrite(data.frame(a = c(1,2,3), b = c("red", "amber", "green")), "testa.rds")
+#' simplewrite(data.frame(a = c(1,2,3, 4), b = c("red", "amber", "green", "blue")), "testb.rds")
+#' files <- list.files(pattern = ".rds")
+#' dt3 <- search_IDs(IDs = c(3,4), IDcol = "a", files = files)
+#' dt4 <- search_IDs(IDs = 4, IDcol = "a", files = files, complete = TRUE)
+#'
+#' @export
+IDinfilesearch <- function(IDs, IDcol, files, complete = FALSE){
+  IDs <- as.character(IDs)
+  t <- list()
+  for (f in files){
+    d <- data.table::data.table(simpleimportforce(f))
+    col <- names(d)[grepl(IDcol, names(d), ignore.case = T)]
+    d[, look:= as.character(d[,get(col)])]
+    x <- d[look%in%IDs,][,look := NULL]
+    t[[f]] <- x
+    message(paste(f, ";",nrow(x), "matching rows"))
+    remove(d, f)
+  }
+  if(complete == FALSE) t <- Filter(f = function(x)nrow(x)>0, x = t)
+
+  return(t)
+}
--- R/simpleimport.R
+++ R/simpleimport.R
@@ -1,4 +1,4 @@
-#' Import various formats of files into R with one command
+#' Import various formats of files into R with one command. If file is not found a brower will open to select the file
 #'
 #' Quick import of file types Rdata, RDS, rda, csv, txt, xlsx, xls or dta into your R environment.
 #'
@@ -63,6 +63,74 @@ simpleimport <- function(file, sheet, skip, ...){
     stop("unsupported file type")
     invisible(readline(prompt="Unsupported file type, Press [enter] to continue or [Esc] to stop and retry"))
     }
+  message(paste0("Imported ", ifelse(grepl("csv|xls|dta|rds", tools::file_ext(file), ignore.case = T), file, paste0(file, " containing ", paste0(ls()[!(ls() %in% c("sheet", "file", "skip", "df"))], collapse = ", ")))))
+  return(df)
+}
+
+
+#' Import various formats of files into R with one command.
+#'
+#' Quick import of file types Rdata, RDS, rda, csv, txt, xlsx, xls or dta into your R environment.
+#'
+#' @param file Optional; file to import. If not in your working directory the path needs to be included. If not specified a browser will open to select a file.
+#'
+#' @param sheet Optional; sheet name or number within workbooks to import. By default the first tab is imported.
+#'
+#' @param skip Optional; Number of rows from top of the spreadsheet to skip in the import.
+#'
+#' @param ... Optional; other import arguments from the fread for csv, readxl for xls or xlsx or haven for dta files.
+#'
+#' @return files imported into R
+#'
+#'
+#' @author Diane Hatziioanou
+#'
+#' @keywords import
+#'
+#' @examples
+#'
+#' # Import a csv file
+#' export("C:..../file.csv")
+#'
+#' # Import a specified sheer from an xlsx file
+#' export("C:..../file.xlsx", sheet = "Data")
+#'
+#' # Import a specified sheet from an xls file skipping the first 4 rows
+#' export("C:..../file.xls", sheet = 4, skip = 4)
+#'
+#' @export
+simpleimportforce <- function(file, sheet, skip, ...){
+  if (missing(skip)) {skip <- 0}
+  if (missing(file)) {
+    stop("File not found, select a file from the browser")
+  }
+  if(!(isTRUE(try(httr::HEAD(file)[2] == 200, silent = T)))){
+    if(!(file.exists(file)) ){
+      stop("File not found, select a file from the browser")
+    }
+  }
+
+  if (grepl("csv|txt", tools::file_ext(file), ignore.case = T)) {
+    df <- data.table::fread(file, header = T, stringsAsFactors = F, showProgress = T, skip = skip, na.strings= c("NA", "NULL", NULL), encoding = "UTF-8", ...)
+  } else if (grepl("xlsx|xlsm", tools::file_ext(file), ignore.case = T)) {
+    if (missing(sheet)) {sheet <- 1}
+    df <- readxl::read_xlsx(file, sheet = sheet,  col_names  =  T, skip = skip, ...) #col_types = "text",
+  } else if (grepl("xls", tools::file_ext(file), ignore.case = T)) {
+    if (missing(sheet)) {sheet <- 1}
+    df <- readxl::read_xls(file, sheet = sheet,  col_names  =  T, col_types = "text", skip = skip, ...)
+  } else if (grepl("dta", tools::file_ext(file), ignore.case = T)) {
+    df <- haven::read_dta(file, encoding = NULL, skip = skip, ...)
+  } else if (grepl("rda|rdata", tools::file_ext(file), ignore.case = T)){
+    load(file, verbose = T)
+    ifelse(length(ls()[!(ls() %in% c("sheet", "file", "skip", "df"))]) > 1,
+           df <- list(get(ls()[!(ls() %in% c("sheet", "file", "skip", "df"))])),
+           df <- get(ls()[!(ls() %in% c("sheet", "file", "skip", "df"))]))
+  } else if (grepl("rds", tools::file_ext(file), ignore.case = T)){
+    df <- readRDS(file)
+  } else {
+    stop("unsupported file type")
+    invisible(readline(prompt="Unsupported file type, Press [enter] to continue or [Esc] to stop and retry"))
+  }
   message(paste0("Imported ", ifelse(grepl("csv|xls|dta|rds", tools::file_ext(file), ignore.case = T), file, paste0(file, " containing ", paste0(ls()[!(ls() %in% c("sheet", "file", "skip", "df"))], collapse = ", ")))))
   return(df)
 }

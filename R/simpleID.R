685f7ba0d3e84df7ca4cdaec5c4589370282f727
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2021-04-27 20:19:36 +0100

    simpleid fix

--- R/simpleID.R
+++ R/simpleID.R
@@ -14,38 +14,30 @@
 #' Number = "NHS_Number"
 #' name <- "GroupID"
 #' col = c("DOB", "name", "surname", "ZIP")
-#' simpleID(x = x, col =  col, name = name)
-#' simpleID(x = x, col =  col, Number = Number, name = name)
+#' x <- simpleID(x = x, col =  col, name = name)
+#' x <- simpleID(x = x, col =  col, Number = Number, name = name)
 #' @export
-simpleID <- function(x, col, Number, name){
-
-  if(missing(name)) name <- "GroupID"
-  if(missing(Number)) Number <- FALSE
-  if(missing(col)) {
-    col <- names(x)[grepl("Birth|DOB|Postcode|ZIP|Name", names(x), ignore.case = T)]
-    message("No columns specified, using ",paste0(col, collapse = ", "))
-    invisible(readline(prompt="Press [enter] to continue or [Esc] to stop and select other columns"))
-  } else {
-    if(sum(!col %in% names(x)) > 0){
-      stop(paste0("Column not found: ", paste0(col[!col %in% names(x)], collapse = ", ")))
-    }
+simpleID <- function (x, col= names(x)[grepl("Birth|DOB|Postcode|ZIP|Name", names(x), ignore.case = T)], Number = FALSE, name ="GroupID") {
+  x <- copy(data.table::as.data.table(x))
+  message("No columns specified, using ", paste0(col, collapse = ", "))
+  if (sum(!col %in% names(x)) > 0) {
+    stop(paste0("Column not found: ", paste0(col[!col %in%
+                                                   names(x)], collapse = ", ")))
   }
-
-  x <- data.table::as.data.table(x)
-  if(Number != FALSE){
-    x[, PII := Reduce(function(...) paste0(...), x[,mget(col)]), .SD[, mget(col)]]
-    x[,Number_ID := .GRP, by=get(Number)]
-    x[is.na(get(Number)), Number_ID := length((max(x$Number_ID, na.rm = T)+1): sum(is.na(x[,get(Number)]), max(x$Number_ID, na.rm = T)))]
-    g <- igraph::graph_from_data_frame(x[,c("PII", "Number_ID")])
+  if (Number != FALSE) {
+    x[, `:=`(PII, Reduce(function(...) paste0(...), x[,
+                                                      mget(col)])), .SD[, mget(col)]]
+    x[, `:=`(Number_ID, .GRP), by = get(Number)]
+    x[is.na(get(Number)), `:=`(Number_ID, length((max(x$Number_ID,
+                                                      na.rm = T) + 1):sum(is.na(x[, get(Number)]), max(x$Number_ID,
+                                                                                                       na.rm = T))))]
+    g <- igraph::graph_from_data_frame(x[, c("PII", "Number_ID")])
     c <- igraph::clusters(g)$membership
-    x[, (name) := c[ x$PII ]]
-    x[ ,`:=`(PII = NULL, Number_ID = NULL)]
-
+    x[, `:=`((name), c[x$PII])]
+    x[, `:=`(PII = NULL, Number_ID = NULL)]
   } else {
-    x[,(name) := .GRP, by=list(get(col))]
+    x[, `:=`((name), .GRP), by = list(get(col))]
   }
   x <- as.data.frame(x)
   return(x)
 }
-
-

4b6258a666af85ddf5bbe3df63a191fb19a33827
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2021-06-04 09:51:11 +0100

    add simpleencode

---
+++ R/simpleencode.R
@@ -0,0 +1,27 @@
+#' Title Change character encoding to UTF-8 with optional case management
+#'
+#' @param x  Character object or vector to encode.
+#' @param case Optional; Default is none (no formatting). Other options are upper, lower or title.
+#'
+#' @return returns the input encided to UTF-8
+#'
+#' @examples
+#' x <- "troublesome character string"
+#' x <- simpleencode(x)
+#'
+#' @export
+simpleencode <- function(x, case = "none"){
+  x <- stringi::stri_encode(x, "", "UTF-8")
+  x <- stringi::stri_trans_general(x, "name-any")
+  x <- iconv(x, to = "UTF-8")
+  if (case == "upper"){
+    x <- toupper(x)
+  } else if(case == "lower"){
+    x <- tolower(x)
+  } else if (case == "title"){
+    x <- str_to_title(x)
+  } else if (case != "none"){
+    stop("font case not recognised")
+  }
+  return(x)
+}

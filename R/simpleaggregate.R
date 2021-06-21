454ee51d8a66b016b62fcff7fc743982eb3845de
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2021-06-21 11:30:40 +0100

    simpleaggregate octamode and Posixct managed. Col argument cleaned

--- R/simpleaggregate.R
+++ R/simpleaggregate.R
@@ -1,17 +1,17 @@
-#' Title aggregate a dataframe or datatable by an id column
+#' Title aggregate a data.frame or data.table to have 1 row per id column
 #'
 #' @param dt data
 #' @param idcol column with unique identifiers to aggregate by
 #' @param disregard Optional; values to remove from aggregation eg NA, NULL, 0, -1. Default is NA.
-#' @param prefkeep Optional; Where columns end up with more than one value should the first value be saved. Deafault is FALSE.
-#' @param col Optional; Columns to process with prefkeep
+#' @param prefkeep Optional; Where columns end up with more than one value should the first value be saved. Default is FALSE.
+#' @param col Optional; Columns to process with prefkeep. "all" will process all columns
 #'
-#' @return
+#' @return Returns data.frame aggregated to one row per idcol.
 #'
 #' @examples
-#'
 #' messy <- data.frame(ID = c(1,1,2,3), data = c("red", "yellow", "red", "green"))
 #' clean <- simpleaggregate(dt = messy, idcol = "ID", disregard=c(NULL,NA,""))
+#' clean <- simpleaggregate(dt = messy, idcol = "ID", disregard=c(NULL,NA,""), prefkeep = 1, col = "all")
 #'
 #' @export
 simpleaggregate <- function(dt, idcol, disregard =NA, prefkeep=FALSE, col){
@@ -29,10 +29,10 @@ simpleaggregate <- function(dt, idcol, disregard =NA, prefkeep=FALSE, col){
   if(prefkeep != FALSE){
     if(length(col)==1){
       if(col == "all"){
-        cols = names(dup)[1:ncol(dup)]
+        col = names(dup)[1:ncol(dup)]
       }
     }
-    dup[ , (cols) := lapply(.SD, function(x) {gsub("^(.*?),.*", "\\1", x)}), .SDcols = cols]
+    dup[ , (col) := lapply(.SD, function(x) {gsub("^(.*?),.*", "\\1", x)}), .SDcols = col]
   }
 
   # Convert previous Date columns back to Date format
@@ -41,7 +41,16 @@ simpleaggregate <- function(dt, idcol, disregard =NA, prefkeep=FALSE, col){
     dup[ , (Date_columns) := lapply(.SD, function(x) {as.character(x)}), .SDcols = Date_columns]
     dup[ , (Date_columns) := lapply(.SD, function(x) {simpledates(x)}), .SDcols = Date_columns]
   }
-
+  Posix_columns <- colnames(dt)[grepl("POSIXct", sapply(dt,class))]
+  if (isTRUE(length(Posix_columns) > 0)) {
+    dup[ , (Posix_columns) := lapply(.SD, function(x) {as.character(x)}), .SDcols = Posix_columns]
+    dup[ , (Posix_columns) := lapply(.SD, function(x) {as.POSIXct(x)}), .SDcols = Posix_columns]
+  }
+  octmode_columns <- colnames(dt)[grepl("octmode", sapply(dt,class))]
+  if (isTRUE(length(octmode_columns) > 0)) {
+    dup[ , (octmode_columns) := lapply(.SD, function(x) {as.numeric(x)}), .SDcols = octmode_columns]
+    dup[ , (octmode_columns) := lapply(.SD, function(x) {as.octmode(x)}), .SDcols = octmode_columns]
+  }
   dt <- dt[!(get(idcol) %in% dup[,get(idcol)]),]
   dt <- data.table::rbindlist(list(dt, dup), use.names = T, fill = T)
 

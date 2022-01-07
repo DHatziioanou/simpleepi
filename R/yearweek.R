ff8f30da2a39f6dbce336585ce7bce906cbbf4e7
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-01-07 09:46:17 +0000

    suppwarnings

--- R/yearweek.R
+++ R/yearweek.R
@@ -13,6 +13,6 @@ yearweek  <- function(x){
   if(any(!(class(x) %in% c("Date", "IDate")))) x <- simpledates(x)
   week <- data.table::isoweek(x)
   year <- data.table::year(x)
-  yearweek <- as.integer(paste0(year,stringr::str_pad(string = week, width = 2, side = "left", pad = 0)))
+  yearweek <- suppressWarnings(as.integer(paste0(year,stringr::str_pad(string = week, width = 2, side = "left", pad = 0))))
   return(yearweek)
 }

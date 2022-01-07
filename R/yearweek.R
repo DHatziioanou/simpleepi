27176b6e9361c1da6b821806df7eb7a15c174131
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-01-07 09:42:31 +0000

    add yearweek

---
+++ R/yearweek.R
@@ -0,0 +1,18 @@
+#' Title Format a date to year-isoweek
+#'
+#' @param x Date
+#'
+#' @return Returns x in format YYYYWW
+#'
+#' @examples
+#' yw <- simpleyearweek(date)
+#' x$yw <- simpleyearweek(x$date)
+#'
+#' @export
+yearweek  <- function(x){
+  if(any(!(class(x) %in% c("Date", "IDate")))) x <- simpledates(x)
+  week <- data.table::isoweek(x)
+  year <- data.table::year(x)
+  yearweek <- as.integer(paste0(year,stringr::str_pad(string = week, width = 2, side = "left", pad = 0)))
+  return(yearweek)
+}

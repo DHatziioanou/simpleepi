b50006b64cbdc47147fa3b6c5b1648438652c48e
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-06-01 13:50:09 +0100

    rename col

--- R/add_age_groups.R
+++ R/add_age_groups.R
@@ -16,11 +16,11 @@
 #' @export
 age_groups <- function(age, groups= c('0-4', "5-10", "11-16", "17-24", "25-30", "31-39", "40-59", "60-79", "80+"), unknowns = c("-1", NA), factor = FALSE){
   if(missing(age)) stop("No age data")
-  a <-data.table::tstrsplit(groups, "-", fill=NA, type.convert=F, names=c("V1","V2"), fixed=TRUE)
-  a <- data.table::data.table(V1 = a$V1, V2 = a$V2)
-  a[,V1 := as.numeric(gsub("+", "", V1, fixed = T))][,V2:=as.numeric(gsub("+", "", V2, fixed = T))][grep("+",groups, fixed = T),V2 := Inf]
+  a <- data.table::tstrsplit(groups, "-", fill=NA, type.convert=F, names=c("L","R"), fixed=TRUE)
+  a <- data.table::data.table(L = a$L, R = a$R)
+  a[,L := as.numeric(gsub("+", "", L, fixed = T))][,R:=as.numeric(gsub("+", "", R, fixed = T))][grep("+",groups, fixed = T),R := Inf]
   a <- cbind(a, groups)
-  out <- sapply(age, FUN = function(x) a[V1 <= x & V2>=x,]$groups, simplify = TRUE)
+  out <- sapply(age, FUN = function(x) a[L <= x & R>=x,]$groups, simplify = TRUE)
   out[sapply(out,function(x) length(x)==0 | any(x %in% unknowns))]<- "unknown age"
   out <- unlist(out)
   if(isTRUE(factor)) out <- factor(out, c(groups, "unknown age"))

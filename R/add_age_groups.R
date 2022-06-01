f343f440655c0e8a03c63e0cf1922e1da86403d5
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-06-01 13:34:14 +0100

    age groups v

--- R/add_age_groups.R
+++ R/add_age_groups.R
@@ -1,8 +1,7 @@
 #' Title Add age group column to data.frame
 #'
 #' @param age  age vector
-#' @param age_groups Input vector of age ranges with upper range marked as x+
-#' @param intermediates values which could go up or down (under development)
+#' @param groups Input vector of age ranges in format "N1-N2" where N1 is the lower age and N2 the upper age. Mark range where max is infinite as "N1+"
 #' @param unknowns vector of Age values to be considered unknowns eg -1, NA, "not stated"
 #' @param factor
 #'
@@ -10,20 +9,20 @@
 #'
 #' @examples
 #' # df <- data.frame(ID = c(1:4), Age = c(8,-1,38,26))
-#' # age_groups <- c('0-4', "5-10", "11-16", "17-24", "25-30", "31-39", "40-59", "60-79", "80+")
-#' # df$group <- add_age_groups(df$Age, age_groups, unknowns, factor)
+#' # groups <- c('0-4', "5-10", "11-16", "17-24", "25-30", "31-39", "40-59", "60-79", "80+")
+#' # df$group <- age_groups(df$Age, groups, unknowns, factor)
 #'
 #' @import data.table
 #' @export
-add_age_groups <- function(age, age_groups= c('0-4', "5-10", "11-16", "17-24", "25-30", "31-39", "40-59", "60-79", "80+"), unknowns = c("-1", NA), factor = FALSE){
+age_groups <- function(age, groups= c('0-4', "5-10", "11-16", "17-24", "25-30", "31-39", "40-59", "60-79", "80+"), unknowns = c("-1", NA), factor = FALSE){
   if(missing(age)) stop("No age data")
-  a <- data.table::data.table(do.call(rbind, base::strsplit(age_groups, "-", fixed=TRUE)))
-  a[,V1 := as.numeric(gsub("+", "", V1, fixed = T))][,V2:=as.numeric(gsub("+", "", V2, fixed = T))][grep("+",age_groups, fixed = T),V2 := Inf]
-  a <- cbind(a, age_groups)
-  out <- sapply(age, FUN = function(x) a[V1 <= x & V2>=x,]$age_groups, simplify = TRUE)
+  a <- data.table::data.table(do.call(rbind, base::strsplit(groups, "-", fixed=TRUE)))
+  a[,V1 := as.numeric(gsub("+", "", V1, fixed = T))][,V2:=as.numeric(gsub("+", "", V2, fixed = T))][grep("+",groups, fixed = T),V2 := Inf]
+  a <- cbind(a, groups)
+  out <- sapply(age, FUN = function(x) a[V1 <= x & V2>=x,]$groups, simplify = TRUE)
   out[sapply(out,function(x) length(x)==0 | any(x %in% unknowns))]<- "unknown age"
   out <- unlist(out)
-  if(isTRUE(factor)) out <- factor(out, c(age_groups, "unknown age"))
+  if(isTRUE(factor)) out <- factor(out, c(groups, "unknown age"))
   return(out)
 }
 

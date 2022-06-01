2ddcb43967a762bc504f8961c1dc735f10289247
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-06-01 13:59:38 +0100

    rename objs

--- R/add_age_groups.R
+++ R/add_age_groups.R
@@ -16,11 +16,11 @@
 #' @export
 age_groups <- function(age, groups= c('0-4', "5-10", "11-16", "17-24", "25-30", "31-39", "40-59", "60-79", "80+"), unknowns = c("-1", NA), factor = FALSE){
   if(missing(age)) stop("No age data")
-  a <- data.table::tstrsplit(groups, "-", fill=NA, type.convert=F, names=c("L","R"), fixed=TRUE)
-  a <- data.table::data.table(L = a$L, R = a$R)
-  a[,L := as.numeric(gsub("+", "", L, fixed = T))][,R:=as.numeric(gsub("+", "", R, fixed = T))][grep("+",groups, fixed = T),R := Inf]
-  a <- cbind(a, groups)
-  out <- sapply(age, FUN = function(x) a[L <= x & R>=x,]$groups, simplify = TRUE)
+  .a <- data.table::tstrsplit(groups, "-", fill=NA, type.convert=F, names=c("L","R"), fixed=TRUE)
+  .a <- data.table::data.table(L = .a$L, R = .a$R)
+  .a[,L := as.numeric(gsub("+", "", L, fixed = T))][,R:=as.numeric(gsub("+", "", R, fixed = T))][grep("+",groups, fixed = T),R := Inf]
+  .a <- cbind(.a, groups)
+  out <- sapply(age, FUN = function(x) .a[L <= x & R>=x,]$groups, simplify = TRUE)
   out[sapply(out,function(x) length(x)==0 | any(x %in% unknowns))]<- "unknown age"
   out <- unlist(out)
   if(isTRUE(factor)) out <- factor(out, c(groups, "unknown age"))

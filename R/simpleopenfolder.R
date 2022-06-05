96a0e525e47d5355e4ecfd6fd3439b88a9710a2b
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-06-05 18:41:08 +0100

    rename openf

--- R/simpleopenfolder.R
+++ R/simpleopenfolder.R
@@ -1,15 +1,16 @@
-#' Title Open File Explorer at a specified path. Only tested for Windows
+#' Title Open File Explorer at a specified path or open files from R.
 #'
-#' @param path Path to open in a File Explorer
+#' @param path Path to folder or file to open
 #'
 #'
-#' @return Opens a File Explorer window at the given path
+#' @return Opens a File Explorer window at the given path or file
 #'
 #' @examples
-#' dir.create("test")
-#' simpleopenfolder("test")
+#' # dir.create("test")
+#' # simpleopen("test")
 #'
 #' @export
-simpleopenfolder <- function(path){
+simpleopen <- function(path){
+  if(!file.exists(path)) stop(paste0("path not found; ", path))
   suppressWarnings(shell(paste0("explorer ", gsub("/", "\\\\", path)), intern = TRUE))
 }

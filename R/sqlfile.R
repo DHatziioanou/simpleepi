83d2fab4e267ecc6a8079732590e47e701dbe798
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2022-06-14 10:28:29 +0100

    sql file read

--- R/sqlfile.R
+++ R/sqlfile.R
@@ -1,3 +1,19 @@
+#' Title Read sql query file
+#'
+#' @param sqlfile sql query file
+#'
+#' @return sql file formatted for use in R
+#'
+#' @examples
+#' query <- sqlfile_read("C:/Users/path/query1.sql")
+#'
+#' @export
+sqlfile_read <- function(sqlfile){
+  q <- glue::glue(paste(readLines(sqlfile, warn = FALSE, skipNul = TRUE, n = -1L), collapse = "\r\n"))
+  return(q)
+}
+
+
 #' Title Use sql query file to query database via odbc
 #'
 #' @param server Server holidng data
@@ -16,7 +32,7 @@ sqlfile_query <- function(server, database, sqlfile, name = "df"){
   start <- Sys.time()
 
   if(!("SQL Server" %in% odbc::odbcListDrivers()$name)) stop("SQL Server driver missing. Install SQLSRV32.DLL then try again")
-  suppressWarnings(q <- glue::glue(paste(readLines(sqlfile), collapse = "\r\n")))
+  q <- sqlfile_read(sqlfile)
   con <- try(odbc::dbConnect(odbc::odbc(),
                              driver="SQL Server",
                              server = server,
@@ -26,7 +42,7 @@ sqlfile_query <- function(server, database, sqlfile, name = "df"){
   } else if(con@info$dbname == database) message("Connected")
 
   # Run query
-  try(assign(name, odbc::dbGetQuery(con, q), envir=.GlobalEnv))
+  try(assign(name, odbc::dbGetQuery(con, q), envir=as.environment(1)))
   odbc::dbDisconnect(con)
 
   if(is.null(nrow(get(name,envir=.GlobalEnv))) | nrow(get(name,envir=.GlobalEnv))==0){
@@ -37,3 +53,8 @@ sqlfile_query <- function(server, database, sqlfile, name = "df"){
   }
   message(paste("Run time",round(Sys.time()-start, 3), units(Sys.time()-start), sep = " "))
 }
+
+
+
+
+

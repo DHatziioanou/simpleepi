caf2e170d07324d862a3383e473f2442366fad8c
Diane Hatziioanou <diane.hatziioanou@phe.gov.uk>
2024-05-16 10:13:48 +0100

    add xsxl+password

--- R/read_write.R
+++ R/read_write.R
@@ -9,6 +9,7 @@
 #' @param skip Optional; Number of rows from top of the spreadsheet to skip in the import.
 #'
 #' @param ... Optional; other import arguments from the fread for csv, readxl for xls or xlsx or haven for dta files.
+#' @param password Password for password-protected workbook.
 #'
 #' @return files imported into R
 #'
@@ -30,7 +31,7 @@
 #' # df <- simpleimport("test.rds")
 #'
 #' @export
-simpleimport <- function(file, sheet, skip, ...){
+simpleimport <- function(file, sheet, skip, password = NULL, ...){
   if (missing(skip)) {skip <- 0}
   if (missing(file)) {
     message("File not found, select a file from the browser")
@@ -47,7 +48,11 @@ simpleimport <- function(file, sheet, skip, ...){
     df <- data.table::fread(file, header = T, stringsAsFactors = F, showProgress = T, skip = skip, na.strings= c("NA", "NULL", NULL), encoding = "UTF-8", ...)
   } else if (grepl("xlsx|xlsm", tools::file_ext(file), ignore.case = T)) {
     if (missing(sheet)) {sheet <- 1}
-    df <- readxl::read_xlsx(file, sheet = sheet,  col_names  =  T, skip = skip, ...) #col_types = "text",
+    if (is.null(password)){
+      df <- readxl::read_xlsx(file, sheet = sheet,  col_names  =  T, skip = skip, ...) #col_types = "text",
+    } else {
+      df <- excel.link::xl.read.file(file, xl.sheet = sheet,  header =  T, password = password, ...) #col_types = "text",
+    }
   } else if (grepl("xls", tools::file_ext(file), ignore.case = T)) {
     if (missing(sheet)) {sheet <- 1}
     df <- readxl::read_xls(file, sheet = sheet,  col_names  =  T, col_types = "text", skip = skip, ...)
@@ -92,6 +97,7 @@ simpleimport <- function(file, sheet, skip, ...){
 #' @param skip Optional; Number of rows from top of the spreadsheet to skip in the import.
 #'
 #' @param ... Optional; other import arguments from the fread for csv, readxl for xls or xlsx or haven for dta files.
+#' @param password Password for password-protected workbook.
 #'
 #' @return files imported into R
 #'
@@ -114,7 +120,7 @@ simpleimport <- function(file, sheet, skip, ...){
 #'
 #'
 #' @export
-simpleimportforce <- function(file, sheet, skip, ...){
+simpleimportforce <- function(file, sheet, skip, password = NULL, ...){
   if (missing(skip)) {skip <- 0}
   if (missing(file)) {
     stop("File not found, select a file from the browser")
@@ -129,7 +135,11 @@ simpleimportforce <- function(file, sheet, skip, ...){
     df <- data.table::fread(file, header = T, stringsAsFactors = F, showProgress = T, skip = skip, na.strings= c("NA", "NULL", NULL), encoding = "UTF-8", ...)
   } else if (grepl("xlsx|xlsm", tools::file_ext(file), ignore.case = T)) {
     if (missing(sheet)) {sheet <- 1}
-    df <- readxl::read_xlsx(file, sheet = sheet,  col_names  =  T, skip = skip, ...) #col_types = "text",
+    if (is.null(password)){
+      df <- readxl::read_xlsx(file, sheet = sheet,  col_names  =  T, skip = skip, ...) #col_types = "text",
+    } else {
+      df <- excel.link::xl.read.file(file, xl.sheet = sheet,  header =  T, password = password, ...) #col_types = "text",
+    }
   } else if (grepl("xls", tools::file_ext(file), ignore.case = T)) {
     if (missing(sheet)) {sheet <- 1}
     df <- readxl::read_xls(file, sheet = sheet,  col_names  =  T, col_types = "text", skip = skip, ...)
@@ -161,6 +171,8 @@ simpleimportforce <- function(file, sheet, skip, ...){
 #' @param type file type to save data as as a string. If not defined the file name extension will be used or "csv" if no extension given. Options are csv, xlsx, xls, rdata, rdsd, dta.
 #'
 #' @param sheet Optional; for xlsx and xls the sheet name to write data to.
+#' @param password Password for password-protected workbook.
+#' @param write.res.password Second password for editing workbook.
 #'
 #' @return writes data to file
 #'
@@ -178,7 +190,7 @@ simpleimportforce <- function(file, sheet, skip, ...){
 #' # simplewrite(data, "C:..../file.xls", sheet = 4)
 #'
 #' @export
-simplewrite <- function(data, file, type, sheet){
+simplewrite <- function(data, file, type, sheet, password = NULL, write.res.password=NULL){
   if (missing(data)) stop("No data to write")
   # File name
   if (missing(file)) {file <- paste0(deparse(substitute(data)), format(Sys.Date(), "%Y%m%d"))}
@@ -190,6 +202,7 @@ simplewrite <- function(data, file, type, sheet){
   } else if (type == "xlsx") {
     if (missing(sheet)) sheet <- paste0(format(Sys.Date(), "%Y%m%d"), " data")
     style <- openxlsx::createStyle(fontSize = 11, halign = "center", border = "TopBottomLeftRight", borderStyle = "thick", fontColour = "black", wrapText = T, textDecoration = "bold")
+    if(is.null(password)){
     wb <- openxlsx::createWorkbook()
     openxlsx::addWorksheet(wb, sheet)
     openxlsx::writeData(wb, sheet, data, startCol = 1, startRow = 1, borders = "surrounding", borderStyle = "thick", headerStyle = style)
@@ -197,6 +210,9 @@ simplewrite <- function(data, file, type, sheet){
     openxlsx::addFilter(wb, sheet, row = 1, cols = 1:ncol(data))
     # SAVE WORKBOOK
     openxlsx::saveWorkbook(wb, paste0(file, ".xlsx"), overwrite = T)
+    } else {
+    excel.link::xl.save.file(r.obj = data, filename = paste0(file, ".xlsx"), row.names = F, col.names = T, xl.sheet = sheet, password = password, write.res.password =write.res.password)
+    }
   } else if (type == "dta") {
     haven::write_dta(data,  paste0(file, ".dta"), version = 14)
   } else if (grepl("rda|rdata", type, ignore.case = T)) {
